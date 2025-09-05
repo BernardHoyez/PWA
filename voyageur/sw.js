1d00e8bb46a858ae8e5b98f02f84f6cc312a4cda
BernardHoyez <bhoyez@gmail.com>
2025-09-05 17:50:36 +0200

    Add files via upload

---
+++ voyageur/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('voyageur-cache-v1').then((cache) => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+});
+self.addEventListener('fetch', (e) => {
+  const url = e.request.url;
+  if(url.includes('tile.openstreetmap.org')) return e.respondWith(fetch(e.request));
+  e.respondWith(caches.match(e.request).then((r)=>r||fetch(e.request)));
+});
