71db671cbb072ae062f748983aab33045ffe2c7f
BernardHoyez <bhoyez@gmail.com>
2025-09-06 15:50:47 +0200

    Delete editeur_visite_sites_corrige directory

--- editeur_visite_sites_corrige/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('editeur-cache').then(cache => cache.addAll(['./','./index.htm'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
