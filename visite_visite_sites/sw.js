7adef19399af8633afa623753a3a0115b0632f73
BernardHoyez <bhoyez@gmail.com>
2025-09-06 17:00:30 +0200

    Delete visite_visite_sites directory

--- visite_visite_sites/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('visite-cache').then(cache => cache.addAll(['./','./index.htm'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
