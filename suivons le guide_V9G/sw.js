c64b1850372d0af7b714bd5a04ae5b0dbf0f0068
BernardHoyez <bhoyez@gmail.com>
2025-09-12 11:30:02 +0200

    Add files via upload

---
+++ suivons le guide_V9G/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'guide-v5-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.tsx',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
