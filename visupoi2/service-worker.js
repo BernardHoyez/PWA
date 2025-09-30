c8db243336936639ce7d65c5a48d8d84dda9e5fa
BernardHoyez <bhoyez@gmail.com>
2025-09-30 13:38:10 +0200

    Delete visupoi2 directory

--- visupoi2/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker installé");
-});
-
-self.addEventListener("fetch", e => {
-  // Laisser passer toutes les requêtes (mode développement)
-});
