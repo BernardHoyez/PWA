7ebb80c27c6e337ecf2bdc01c1decb09918dd6bc
BernardHoyez <bhoyez@gmail.com>
2025-09-30 08:03:12 +0200

    Delete visupoi2 directory

--- visupoi2/service-worker.js
+++
@@ -1,3 +0,0 @@
-// Service worker basique – tu peux le supprimer si tu préfères sans cache
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
