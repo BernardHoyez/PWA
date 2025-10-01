c5480944576fb4301527f99879dfd582bd552db0
BernardHoyez <bhoyez@gmail.com>
2025-10-01 13:19:46 +0200

    Delete visupoi3 directory

--- visupoi3/js/zip-handler.js
+++
@@ -1,8 +0,0 @@
-// Inclusion de JSZip via CDN
-// Ce fichier peut rester vide si tu inclus directement le CDN dans index.html
-// Exemple : <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-
-// Ici, on ne fait que vérifier que JSZip est bien dispo
-if (typeof JSZip === "undefined") {
-  console.error("JSZip n'est pas chargé. Vérifie ton index.html");
-}
