d0476a358f8b98b4a806316eaf249b6ba78feb00
BernardHoyez <bhoyez@gmail.com>
2025-10-01 15:37:06 +0200

    Delete visupoi3 directory

--- visupoi3/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  initMap();
-
-  const fileInput = document.getElementById('zipFile');
-  fileInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    if (file) handleZip(file);
-  });
-});
