a7a284a01777b550f751d7ebbe49cb9d939d8e89
BernardHoyez <bhoyez@gmail.com>
2025-10-08 17:48:00 +0200

    Add files via upload

---
+++ ordonnepoi/sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='ordonnepoi-v1';
+const CORE=['./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
