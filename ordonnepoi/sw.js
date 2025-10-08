592466059b7fcfb70b926c1ad5913cc792790f44
BernardHoyez <bhoyez@gmail.com>
2025-10-08 18:25:37 +0200

    Delete ordonnepoi directory

--- ordonnepoi/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='ordonnepoi-v2';
-const CORE=['./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
