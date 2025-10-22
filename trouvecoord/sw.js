739fe060af59aaed436501fba748f0500f1df403
BernardHoyez <bhoyez@gmail.com>
2025-10-22 16:16:54 +0200

    Add files via upload

---
+++ trouvecoord/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'trouvecoord-v1';
+const urlsToCache = [
+  '/PWA/trouvecoord/',
+  '/PWA/trouvecoord/index.html',
+  '/PWA/trouvecoord/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/exif-js/2.3.0/exif.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
