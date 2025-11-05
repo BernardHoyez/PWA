56d057f46dd6676439b2cb1dbb97afdb089d9f87
BernardHoyez <bhoyez@gmail.com>
2025-11-05 23:20:31 +0100

    Delete rando directory

--- rando/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('rando-cache').then(cache => cache.addAll(['.', 'index.html', 'style.css', 'app.js', 'manifest.json'])));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
