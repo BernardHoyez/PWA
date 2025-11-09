ef4a38e1d1edfa8083e22d85e7a3ccf9e3d72991
BernardHoyez <bhoyez@gmail.com>
2025-11-09 23:44:19 +0100

    Add files via upload

---
+++ convertisseur_md/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'excel-md-cache-v1';
+const FILES_TO_CACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
