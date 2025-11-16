cc47cb63ebc07d4574535c5f1b9c20482ac65415
BernardHoyez <bhoyez@gmail.com>
2025-11-16 20:28:05 +0100

    Delete randonne directory

--- randonne/sw-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-      .then(() => console.log('Service Worker enregistré'))
-      .catch(err => console.error('Erreur enregistrement SW:', err));
-  });
-}
