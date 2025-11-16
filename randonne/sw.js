0e7648303e0d931bb06a7471f4315d35c9e20cb7
BernardHoyez <bhoyez@gmail.com>
2025-11-16 20:04:36 +0100

    Add files via upload

---
+++ randonne/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'randonne-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/PWA/randonne/index.html',
+  '/PWA/randonne/styles.css',
+  '/PWA/randonne/main.js',
+  '/PWA/randonne/manifest.json',
+  '/PWA/randonne/icon192.png',
+  '/PWA/randonne/icon512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
