5d18ec8d146e7a89cd49131b85073b27359c9726
BernardHoyez <bhoyez@gmail.com>
2025-11-17 13:33:25 +0100

    Add files via upload

---
+++ matrace2/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'matrace2-v1';
+const urlsToCache = [
+  '/PWA/matrace2/',
+  '/PWA/matrace2/index.html',
+  '/PWA/matrace2/style.css',
+  '/PWA/matrace2/app.js',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
