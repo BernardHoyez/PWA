abba8c40d4d647cb4b1ad969f1a34f90bfc74c94
BernardHoyez <bhoyez@gmail.com>
2025-11-26 22:23:06 +0100

    Delete tracecarte directory

--- tracecarte/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open("tracecache").then(c=>c.addAll([
-   "./","./index.html","./style.css","./app.js","./manifest.webmanifest"
- ])));
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
