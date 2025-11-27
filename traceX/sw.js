61bc073d0c2342df7d32384173465f62e4ca3e79
BernardHoyez <bhoyez@gmail.com>
2025-11-27 09:31:15 +0100

    Delete traceX directory

--- traceX/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'traceX-v1';
-const urls = ['/PWA/traceX/','/PWA/traceX/index.html','/PWA/traceX/app.js','/PWA/traceX/manifest.json'];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
