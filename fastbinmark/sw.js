8b274f5f5ff9fc1b468d1078c2cf54abf97e4dcf
BernardHoyez <bhoyez@gmail.com>
2025-12-06 23:55:31 +0100

    Add files via upload

---
+++ fastbinmark/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("fastbinmark-cache").then(cache => cache.addAll([
+      "./","./index.html","./main.js","./manifest.json"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
