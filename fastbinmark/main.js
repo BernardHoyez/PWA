f85eb35eb44229cbefc81abaeea0ce9518fc4249
BernardHoyez <bhoyez@gmail.com>
2025-12-07 00:05:02 +0100

    Add files via upload

---
+++ fastbinmark/main.js
@@ -0,0 +1,6 @@
+function loadFastbin(){
+  const url=document.getElementById('fastbinLink').value;
+  if(!url){ alert('Entrez un lien fastbin'); return; }
+  console.log('Chargement fastbin:',url);
+  // TODO: implement fastbin loading
+}
