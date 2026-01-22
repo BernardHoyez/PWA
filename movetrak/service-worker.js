f353c4b9df594dfc942047e779ca4ade7efcf7ba
BernardHoyez <bhoyez@gmail.com>
2026-01-22 09:23:49 +0100

    Add files via upload

---
+++ movetrak/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'movetrak-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon192.png',
+  './icon512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
