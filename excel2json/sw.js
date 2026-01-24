fe20e665c6022068a26e6b518ac4902ad3acf9af
BernardHoyez <bhoyez@gmail.com>
2026-01-24 09:00:15 +0100

    Add files via upload

---
+++ excel2json/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'excel2json-v1';
+const urlsToCache = [
+  '/PWA/excel2json/',
+  '/PWA/excel2json/index.html',
+  '/PWA/excel2json/manifest.json',
+  '/PWA/excel2json/icon192.png',
+  '/PWA/excel2json/icon512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+```
+
