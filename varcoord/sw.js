b55ef9c687fbafc37c7afdc7717fdb3c2f71ee95
BernardHoyez <bhoyez@gmail.com>
2026-01-29 20:10:36 +0100

    Add files via upload

---
+++ varcoord/sw.js
@@ -0,0 +1,21 @@
+const CACHE = "varcoord-cache-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon192.png",
+        "./icon512.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
