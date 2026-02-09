fc5ffce8724258b680de4c8e8dea6479087f2562
BernardHoyez <bhoyez@gmail.com>
2026-02-09 20:18:34 +0100

    Delete fastbinmark directory

--- fastbinmark/main.js
+++
@@ -1,6 +0,0 @@
-function loadFastbin(){
-  const url=document.getElementById('fastbinLink').value;
-  if(!url){ alert('Entrez un lien fastbin'); return; }
-  console.log('Chargement fastbin:',url);
-  // TODO: implement fastbin loading
-}
