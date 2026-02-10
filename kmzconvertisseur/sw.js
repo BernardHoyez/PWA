c40ca19c2f379f0c6184fd2fdd357afcd58ca592
BernardHoyez <bhoyez@gmail.com>
2026-02-10 22:37:17 +0100

    Add files via upload

---
+++ kmzconvertisseur/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'kmzconvertisseur-v1';
+const CACHE_FILES = [
+  './',
+  'index.html',
+  'manifest.json',
+  'icon192.png',
+  'icon512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHE_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
